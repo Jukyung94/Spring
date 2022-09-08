fc326f2dbfd3fd507ea8010ec6d2d5b243c794e5
Jukyung <sungjukyung94@gmail.com>
2022-09-08 23:52:07 +0900

    practice @pathVariable

--- SampleApp-1/src/main/java/com/study/HeloController.java
+++ SampleApp-1/src/main/java/com/study/HeloController.java
@@ -2,6 +2,7 @@ package com.study;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController //Annotation to use Rest Controller
 public class HeloController {
@@ -9,6 +10,15 @@ public class HeloController {
 	@RequestMapping("/") //annotation to mapping url
 	public String index() {
 		return "Hello Spring-Boot World";
+	}
+	
+	@RequestMapping("/{num}") //annotation with variables to create paths
+	public String pagebyNum(@PathVariable int num) {
+		int res = 0;
+		for(int i = 1; i <= num; i++) {
+			res += i;
+		}
+		return "total: " + res;
 	}
 	
 

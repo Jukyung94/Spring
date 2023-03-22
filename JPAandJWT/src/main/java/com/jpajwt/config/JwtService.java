bbc15f552a78e949524a3598f981f82ddaa42340
Jukyung <sungjukyung94@gmail.com>
2023-03-22 23:16:07 +0900

    generateToken

--- JPAandJWT/src/main/java/com/jpajwt/config/JwtService.java
+++ JPAandJWT/src/main/java/com/jpajwt/config/JwtService.java
@@ -1,11 +1,17 @@
 package com.jpajwt.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -16,7 +22,32 @@ public class JwtService {
 
 	public String extractUsername(String token) {
 		// TODO Auto-generated method stub
-		return null;
+		return extractClaim(token, Claims::getSubject); //subject는 email or username
+	}
+	
+	//extract single claim that we pass
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) { //토큰과 Claim type의 function을 사용한다
+		final Claims claims = extractAllClaims(token); //extract all claims
+		return claimsResolver.apply(claims); //extract from my token
+	}
+	
+	//generate token without extraclaims
+	public String generateToken(UserDetails userDetails) {
+		return generateToken(new HashMap<>(), userDetails);
+	}
+	
+	
+	//generate token method
+	public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+		//extraClaims는 
+		return Jwts
+				.builder()
+				.setClaims(extraClaims) //클레임 설정
+				.setSubject(userDetails.getUsername()) //userDetail에 저장된 username  정보
+				.setIssuedAt(new Date(System.currentTimeMillis())) //현재시간
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) //만료시간
+				.signWith(getSignInKey(), SignatureAlgorithm.HS256) //보안서명
+				.compact(); //빌드
 	}
 	
 	private Claims extractAllClaims(String token) {
@@ -26,7 +57,6 @@ public class JwtService {
 			.build()
 			.parseClaimsJws(token) //parse token
 			.getBody(); //get all the claims that we have within the token
-			
 	}
 
 	private Key getSignInKey() {
